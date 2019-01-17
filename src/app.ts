a6e3603f5e4eed26e7dd8a18f90372f2d60a67d0
vrum <dev@motome.com.au>
2019-01-18 09:48:27 +1100

    Adds an advert delay

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,8 @@ const songs: Array<{
 const i = Math.floor(random.range(0, songs.length - 1));
 const song = songs[i];
 const secondsTillStandup = 10 * 60;
-const secondsForSong = song.seconds + (song.minutes * 60);
+const advertDelay = 30;
+const secondsForSong = song.seconds + advertDelay + (song.minutes * 60);
 const timeoutDelay: number = secondsTillStandup - secondsForSong;
 const delayMinutes: string = ((timeoutDelay / 60) as number).toPrecision(2);
 const delaySeconds: number = (timeoutDelay % 60);
